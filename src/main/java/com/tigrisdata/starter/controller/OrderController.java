9fb320e44035191060721837bcce09d919e87728
Jigar Joshi <jigarjm@gmail.com>
2022-04-12 20:19:04 -0700

    fix: Fixed the transactional collection

--- src/main/java/com/tigrisdata/starter/controller/OrderController.java
+++ src/main/java/com/tigrisdata/starter/controller/OrderController.java
@@ -15,14 +15,21 @@ package com.tigrisdata.starter.controller;
 
 import com.tigrisdata.db.client.error.TigrisDBException;
 import com.tigrisdata.db.client.model.Filters;
-import com.tigrisdata.db.client.service.TigrisCollection;
+import com.tigrisdata.db.client.model.TransactionOptions;
+import com.tigrisdata.db.client.model.UpdateFields;
 import com.tigrisdata.db.client.service.TigrisDatabase;
+import com.tigrisdata.db.client.service.TransactionSession;
+import com.tigrisdata.db.client.service.TransactionTigrisCollection;
 import com.tigrisdata.starter.generated.Order;
 import com.tigrisdata.starter.generated.Product;
 import com.tigrisdata.starter.generated.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -32,22 +39,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class OrderController {
   private final TigrisDatabase tigrisStarterDatabase;
   private final AtomicInteger orderIdSequence;
-  private final TigrisCollection<User> userCollection;
-  private final TigrisCollection<Product> productCollection;
-  private final TigrisCollection<Order> orderCollection;
-
 
   public OrderController(TigrisDatabase tigrisStarterDatabase) {
     this.tigrisStarterDatabase = tigrisStarterDatabase;
     this.orderIdSequence = new AtomicInteger();
-
-    this.userCollection = tigrisStarterDatabase.getCollection(User.class);
-    this.productCollection = tigrisStarterDatabase.getCollection(Product.class);
-    this.orderCollection = tigrisStarterDatabase.getCollection(Order.class);
   }
 
   @GetMapping("/{id}")
   public Order read(@PathVariable("id") int id) throws TigrisDBException {
-    return orderCollection.readOne(Filters.eq("id", id)).get();
+    return tigrisStarterDatabase.getCollection(Order.class).readOne(Filters.eq("id", id)).get();
   }
 }
